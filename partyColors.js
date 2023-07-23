ff5177f0a7cf1617ef11675b59b1a8250a49aed5
Saigesp <saigesp@gmail.com>
2023-07-23 13:55:51 +0200

    add more party colors

--- partyColors.js
+++ partyColors.js
@@ -1,10 +1,8 @@
 window._partyColors = {
+    "UP": "#e51c55",
     "SUMAR": "#e51c55",
-    "PSOE": "#e02020",
-    // "PSVPSOE": "#e02020",
-    // "CP": "#e02020",
-    // "PSC": "#e02020",
-    "PP": "#02a2dd",
+    "PSOE": "#e02020", // #DA291C
+    "PP": "#02a2dd", // #17589D
     "VOX": "#7CBD2A",
     "CS": "#ea8046",
     "JXCATJUNTS": "#20C0B2",
@@ -13,17 +11,24 @@ window._partyColors = {
     "PAR": "#fea000",
     "NA+": "#1c699C",
     "GBAI": "#F75E42",
-    "PNV": "#16823b",
-    "EHBILDU": "#c7d892",
+    "PNV": "#16823b", // #00902F
+    "EHBILDU": "#c7d892", // #08A3A6
     "PRC": "#C2CE0A",
     "FORO": "#17375e",
-    "BNG": "#95ccf7",
-    "CCAPNCNC": "#FEED15",
-    // "NCA": "#85BD42",
+    "BNG": "#76B2E1",
+    "CCA": "#FEED15", // #00ADE9
+    "CCAPNCNC": "#FEED15", // #00ADE9
     "UPL": "#C71585",
     "PACMA": "#00FF7F",
-    // "UPN": "#F19DC3",
-    // "PUMJ": "#0CB4CE",
-    // "CC": "#B0D7EB",
     "TERUELEXISTE": "#007F51",
+    "PUMJ": "#F9CB9C",
+    
+    "PCTE": "#E3061A",
+    "EXISTE": "#007F51",
+    "ADELANTEANDALUCIA": "#24C87E",
+    "CAMINANDOJUNTOS": "#233B90",
+    "CPM": "#719430", // Coalición por melilla
+    "FO": "#B11E1A", // Frente obrero
+    "JM": "#286B3D", // Jaén merece más
+
 }
